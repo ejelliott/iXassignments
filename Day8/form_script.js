63c51a600313fd5f0d0fdbcfd7c4b2c05a9cf5c8
E.J. Elliott <ejelliott@MacBook-Pro-4.local>
2016-06-08 20:09:21 +0200

    Assignment 1 done

---
+++ Day8/form_script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+	$("#submit-btn").click(function() {
+		phoneValidator($("#phone").val());
+		emailValidator($("#email").val());
+	});
+});
+
+function phoneValidator(phoneNum) {
+	// console.log("testgood");
+//check that phone number is in xxx-xxx-xxxx
+var phoneTest = phoneNum.split('-');;
+if(phoneTest.length !== 3) {
+	console.log("no");
+	$("#test1").text("no");
+}
+else if(phoneTest[0].length !== 3) {
+	console.log("no");
+	$("#test1").text("no");
+}
+else if(phoneTest[1].length !== 3) {
+	console.log("no");
+	$("#test1").text("no");
+}
+else if(phoneTest[2].length !== 4) {
+	console.log("no");
+	$("#test1").text("no");
+}
+else {
+	console.log("yes");
+	$("#test1").text("yes");
+}
+}
+
+function emailValidator(email) {
+	// var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+ // 	var valid = emailReg.test(email);
+ // 	if(!valid) {
+ // 		console.log("no");&amp;
+ // 	}
+ // 	else {
+ // 		console.log("yes");
+ // 	}
+ var emailTest1 = email.split('@');
+// var emailTest2 = emailTest1[1].split( '.' );
+if (emailTest1.length !== 2) {
+	console.log("no");
+	$("#test2").text("no");
+}
+// if (emailTest2.length !== 2) {
+// 	console.log("no");
+// }
+else {
+	var emailTest2 = emailTest1[1].split( '.' );
+	if (emailTest2.length !== 2) {
+		console.log("no");
+		$("#test2").text("no");
+	}
+	else {
+		console.log("yes");
+		$("#test2").text("yes");
+	}
+}
+// check that email is in something@something.som
+}
+//phoneValidator(phoneNum), emailValidator(email)
