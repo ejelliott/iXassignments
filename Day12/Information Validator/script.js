e52127debdcafa68f5230de488dc304baa17543a
E.J. Elliott <ejelliott@MacBook-Pro-5.local>
2016-06-15 12:05:09 +0200

    Input field color box changes to red when error

--- Day12/Information Validator/script.js
+++ Day12/Information Validator/script.js
@@ -17,10 +17,27 @@ app.controller("FormCtrl", function($scope){
 		//name test
 		if($scope.name){
 			$scope.count = $scope.count + 1;
+			$scope.myStyleName = {
+				"border-color" : "null"
+			}
+			// $scope.myStyle1 = {
+			// 	"border-color" : "blue"
+			// }
+			// console.log($scope.myStyle1);
+			// $scope.myStyle =
 			// console.log($scope.name);
 		}
 		else{
 			$scope.warnings.push("Please submit a name");
+			$scope.myStyleName = {
+				"border-color" : "red"
+			}
+			// $scpoe.myStyle = {
+			// 	"background-color" : "red"
+			// }
+			// console.log($scpoe.myStyle);
+			// console.log(myStyle);
+			// $scope.myStyle={'border-color':'red'};
 			// console.log("it knows where it is supposed to go")
 		}
 
@@ -28,28 +45,46 @@ app.controller("FormCtrl", function($scope){
 
 		if(!$scope.phone){
 			$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");
+			$scope.myStylePhone = {
+				"border-color" : "red"
+			}
 		}
 		else{
 			$scope.phoneTest = $scope.phone.split('-');	
 
 			if($scope.phoneTest.length !== 3 ) {
-				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");	
+				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");
+				$scope.myStylePhone = {
+				"border-color" : "red"
+			}	
 			}
 
 			else if(($scope.phoneTest[0].length !== 3)||($scope.phoneTest[0] === NaN)) {
-				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");	
+				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");
+				$scope.myStylePhone = {
+				"border-color" : "red"
+			}	
 			}
 
 			else if(($scope.phoneTest[1].length !== 3)||($scope.phoneTest[1] === NaN)) {
 				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");	
+				$scope.myStylePhone = {
+				"border-color" : "red"
+			}
 			}
 
 			else if(($scope.phoneTest[2].length !== 4)||($scope.phoneTest[2] === NaN)) {
-				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");	
+				$scope.warnings.push("Phone Number must be XXX-XXX-XXXX");
+				$scope.myStylePhone = {
+				"border-color" : "red"
+			}	
 			}
 
 			else {
 				$scope.count = $scope.count + 1;
+				$scope.myStylePhone = {
+				"border-color" : "null"
+			}
 			}
 
 		}
@@ -58,19 +93,31 @@ app.controller("FormCtrl", function($scope){
 
 		if(!$scope.email){
 			$scope.warnings.push("Email must be xxxx@xxxx.xx");
+			$scope.myStyleEmail = {
+				"border-color" : "red"
+			}
 		}
 		else {
 			$scope.emailTest1 = $scope.email.split('@');
 			if ($scope.emailTest1.length !== 2) {
 				$scope.warnings.push("Email must be xxxx@xxxx.xx");
+				$scope.myStyleEmail = {
+				"border-color" : "red"
+			}
 			}
 			else {
 				$scope.emailTest2 = $scope.emailTest1[1].split( '.' );
 				if ($scope.emailTest2.length !== 2) {
 					$scope.warnings.push("Email must be xxxx@xxxx.xx");
+					$scope.myStyleEmail = {
+				"border-color" : "red"
+			}
 				}
 				else {
 					$scope.count = $scope.count + 1;
+					$scope.myStyleEmail = {
+				"border-color" : "null"
+			}
 				}
 			}
 
@@ -79,36 +126,58 @@ app.controller("FormCtrl", function($scope){
 		//password test
 		if(!$scope.password) {
 			$scope.warnings.push("Password must be at least 6 characters");
+			$scope.myStyleP = {
+				"border-color" : "red"
+			}
 		}
 		else {
 			if($scope.password.length >= 6){
 				$scope.count = $scope.count + 1;
+				$scope.myStyleP = {
+				"border-color" : "null"
+			}
 			}
 			else{
 				$scope.warnings.push("Password must be at least 6 characters");
+				$scope.myStyleP = {
+				"border-color" : "red"
+			}
 			}
 		}
 
 		//same password test
 		if(!$scope.passwordR ) {
 			$scope.warnings.push("Password must match");
+			$scope.myStylePR = {
+				"border-color" : "red"
+			}
 		}
 		else {
 			if($scope.passwordR.length !== $scope.password.length){
 				$scope.warnings.push("Password must match");
+				$scope.myStylePR = {
+				"border-color" : "red"
+			}
 			}
 			else {
 				for ($scope.i = 0; $scope.i < $scope.passwordR.length; $scope.i = $scope.i + 1) {
 					if ($scope.passwordR[$scope.i] !== $scope.password[$scope.i]){
 						$scope.warnings.push("Password must match");
+						$scope.myStylePR = {
+				"border-color" : "red"
+			}
 					}
 					else {
 						$scope.count = $scope.count + 1;
+						$scope.myStylePR = {
+				"border-color" : "null"
+			}
 					}
 				}
 			}
 			
 		}
+		// console.log($scope.myStyle);
 
 	}
 	
